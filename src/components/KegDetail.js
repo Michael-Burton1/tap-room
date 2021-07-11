a4a76a15098106c981c6b7b12eaf381ac045eb9c
Michael Burton <micbur1@gmail.com>
2021-07-10 22:24:11 -0700

    fixed all referance errors and project builds

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function KegDetail(props) {
-  const { merch, onClickingDelete } = props;
+  const { keg, onClickingDelete } = props;
 
   return (
     <React.Fragment>
       <h1>Keg Details</h1>
-      <h3>{keg.name} -{keg.type}</h3>
-      <h4><em>{keg.type}</em> number of Pints remaining in this keg:<em><strong>{keg.remaining}</strong></em></h4>
+      <h3>{keg.name} - {keg.type}</h3>
+      <h4><em>{keg.abv}</em> number of Pints remaining in this keg:<em><strong>{keg.remaining}</strong></em></h4>
       <button onClick={() => onClickingDelete(keg.id)}>Delete this keg</button>
       <hr />
     </React.Fragment>
@@ -17,8 +17,8 @@ function KegDetail(props) {
 
 KegDetail.propTypes = {
   keg: PropTypes.object,
-  onClickingDelete: PropTypes.func
-  // onClickingEdit: PropTypes.func
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
 };
 
 export default KegDetail;
